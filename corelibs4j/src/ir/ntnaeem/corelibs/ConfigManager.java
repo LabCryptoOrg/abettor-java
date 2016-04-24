17f971d578d5ab10a28ec9ee7f464388c8acca06
Kamran Amini <kam.cpp@gmail.com>
2016-04-24 13:19:02 +0430

    ConfigManager is almsot refactored.

--- corelibs4j/src/ir/ntnaeem/corelibs/ConfigManager.java
+++ corelibs4j/src/ir/ntnaeem/corelibs/ConfigManager.java
@@ -8,24 +8,20 @@ import java.util.List;
 import java.util.Map;
 
 public class ConfigManager {
-
-  private List<String> sections = new ArrayList<String>();
-  private Map<String, List<String>> sectionLines = new HashMap<String, List<String>>();
+  private List<String> sections = new ArrayList<>();
+  private Map<String, List<String>> sectionLines = new HashMap<>();
   private BufferedReader reader;
-
   public ConfigManager(String filePath) throws IOException, SyntaxErrorConfigFile {
     openFile(filePath);
     parse();
   }
-
   public boolean hasSection(String section){
     return sections.contains(section);
   }
-
-  public boolean hasAttribute(String attribute) {
+  public boolean hasValue(String value) {
     for(String section : sections){
       for(String line : sectionLines.get(section)){
-        if(line.contains(attribute)){
+        if(line.contains(value)){
           return true;
         }
       }
@@ -37,24 +33,21 @@ public class ConfigManager {
     return sectionLines.get(sectionName);
   }
 
-  public Map<String,String> getSectionAttributes(String sectionName){
-    Map<String, String> attrValueMap = new HashMap<String , String>();
+  public Map<String,String> getSectionValues(String sectionName){
+    Map<String, String> attrValueMap = new HashMap<>();
     List<String> lines = sectionLines.get(sectionName);
     for(String line : lines){
-      String[] split = line.split(":");
-      String attr = "";
+      String[] split = line.split("[:=]");
+      String attr;
       String value = "";
       attr = split[0].trim();
       if(split.length == 2) {
         value = split[1].trim();
       }
       attrValueMap.put(attr,value);
-      //
     }
     return attrValueMap;
   }
-
-
   private void openFile(String filePath) throws FileNotFoundException {
     File file = new File(filePath);
     reader = new BufferedReader(new FileReader(file));
@@ -70,10 +63,9 @@ public class ConfigManager {
           lines = new ArrayList<>();
         }
         sections.add(line.replace("[","").replace("]",""));
-
       } else {
         if(sections.size() == 0){
-          throw new SyntaxErrorConfigFile("config parameter without section ");
+          throw new SyntaxErrorConfigFile("A value was found that it has no section.");
         }else {
           if(line.length() != 0 &&
              line.charAt(0) != '#') {
@@ -86,15 +78,4 @@ public class ConfigManager {
       sectionLines.put(sections.get(sections.size() - 1) , lines);
     }
   }
-
-//  public static void main(String[] args) throws IOException, SyntaxErrorConfigFile {
-//    ConfigManager configManager = new ConfigManager("/opt/naeem/kawthar/hbc/server/hbc_service.conf");
-//    System.out.println(configManager.hassection("GENERAL"));
-//    System.out.println(configManager.hassection("SERVICE_LIST"));
-//    System.out.println(configManager.hasAttribute("ip"));
-//    Map<String, String> generalAttr = configManager.getsectionAttributes("GENERAL");
-//    System.out.println(generalAttr.get("naeem-gate-ip"));
-//
-//  }
-
 }
